fa4607afc745ea6f5c9e2ba9efaee869266c2e92
Natthanicha Thanavanakul <natthanicha.tha@gmail.com>
2024-06-13 11:32:30 +0200

    completed solution

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+//my solution
+app.get("/api/whoami", function (req, res) {
+  const { headers } = req;
+  res.json({"ipaddress": req.ip, "language": headers['accept-language'], "software": headers['user-agent']});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
